e28e1b202b37682f0d76474df5ef63c205b9c19a
Amrendra <leostereo1108@gmail.com>
2024-12-21 17:27:42 +0530

    integrated User authentication!
    We used clerk and clerk middleware, to set-up the routing.

---
+++ cloudinaryforsass/app/(app)/home/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className="flex flex-auto mx-12 justify-center items-center h-screen">
+      <h1 className="text-2xl font-bold text-gray-200">
+        Home Page is currently under construction🦁
+      </h1>
+    </div>
+  );
+}
+
+export default Home
