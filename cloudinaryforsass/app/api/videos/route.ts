578f0e9048382ca40fd436b35782326cfff30414
Amrendra <leostereo1108@gmail.com>
2024-12-23 02:21:34 +0530

    new correction.

--- cloudinaryforsass/app/api/videos/route.ts
+++ cloudinaryforsass/app/api/videos/route.ts
@@ -3,19 +3,18 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export async function GET(request: NextRequest) {
+export async function GET() {
   try {
     const videos = await prisma.video.findMany({
       orderBy: { createdAt: "desc" },
     });
-
     return NextResponse.json(videos);
   } catch (error) {
     return NextResponse.json(
-      { error: "Error while getting videos" },
+      { error: "Error fetching videos" },
       { status: 500 }
     );
   } finally {
-    await prisma.$disconnect()
+    await prisma.$disconnect();
   }
 }
